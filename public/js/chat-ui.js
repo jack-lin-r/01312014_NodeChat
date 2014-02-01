771032596537b0722f13506c20aedb4324a48e15
Jack Lin <f24u23@berkeley.edu>
2014-02-01 10:27:17 -0800

    allowed multiple user connection

---
+++ public/js/chat-ui.js
@@ -0,0 +1,28 @@
+$(function(){
+
+	var socket = io.connect();
+	$('#choose-nickname').submit(function(e){
+		e.preventDefault();
+		var nick = $('#nickname').val();
+		socket.emit('nickname', nick, function(err){
+			if (!err){
+				$('#nickname-container').hide();
+				$('#chat-container').show();
+			}
+			$('#nick-error').html(err);
+		});
+	});
+
+	socket.on('new nicknames', function(data){
+		 var text = '';
+		 for(var i = 0; i < data.length; i++){
+		 	text += '<span class="user">' + data[i] + "</span>";
+		 }
+		 $('#users').html(text);
+	});
+
+});
+
+
+
+
---
+++ server.js
@@ -0,0 +1,26 @@
+
+var http = require('http'),
+	express = require('express'),
+	chatServer = require('./lib/chat-server.js');
+
+
+
+var app = express();
+var server = http.createServer(app).listen('3000', '127.0.0.1');
+
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+
+	res.sendfile(__dirname + '/views/index.html')
+
+});
+
+
+chatServer.listen(server);
+
+
+
+
+
+
